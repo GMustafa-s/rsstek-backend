ca17ea90a94ab6baffb75a09f0320b321360c065
MuhammadZeeshan444 <76864014+MuhammadZeeshan444@users.noreply.github.com>
2022-06-24 18:45:09 +0500

    translate dynamic pages

--- config/database.php
+++ config/database.php
@@ -58,7 +58,6 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => 'innoDB',
-            'engine' => 'InnoDB',
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
